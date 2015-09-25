14102aba2db2af41970c6984cccbc37ef5bbe124
John Santerre <john@johnsanterre.com>
2015-09-25 12:18:16 -0500

    all tests passed

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,9 +4,9 @@ import unittest
 test_modules = ['test_read',
                 'test_utils',
                 'test_modify',
-                'test_display']
-#                'test_grid_search',
-#                'test_modify']
+                'test_display',
+                'test_grid_search']
+
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.loadTestsFromNames(test_modules)
     unittest.TextTestRunner().run(suite)

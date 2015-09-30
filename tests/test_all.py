5988a8141df5ede493df65e661fb140d4f692afc
zar1 <zachary.rubenstein@gmail.com>
2015-09-30 15:21:55 -0500

    added test_array_emitter to suite

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,8 @@ test_modules = ['test_read',
                 'test_utils',
                 'test_modify',
                 'test_display',
-                'test_grid_search']
+                'test_grid_search',
+                'test_array_emitter']
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.loadTestsFromNames(test_modules)

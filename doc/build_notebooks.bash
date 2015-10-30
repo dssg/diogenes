d935c3aa4d55406a43e66a1b8bb3f63dbfcf79a5
zar1 <zachary.rubenstein@gmail.com>
2015-10-30 10:55:22 -0500

    working on read module notebook. Fixing build to make linking work`

--- doc/build_notebooks.bash
+++ doc/build_notebooks.bash
@@ -1,2 +1,9 @@
 cd notebooks
+# Convert notebooks to rst so we can convert them into html with sphinx's 
+# style
 ipython nbconvert *.ipynb --to rst
+# When we write a sphinx-style intra-project link (ie :mod:`my_module`)
+# in Ipython Notebook in a markdown box, and then convert to an rst, it ends
+# up with double backticks (ie :mod:``my_module``). The below command changes
+# it back so the link will work when we build the documentation
+find . -type f -name '*.rst' -exec sed -E -i '' 's/:(mod|func|data|const|class|meth|attr|exc|obj):``(.*)``/:\1:`\2`/g' {} \;
